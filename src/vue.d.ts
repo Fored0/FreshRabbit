8b64201f7e660380930defdaccc3a0c54266205a
dxj <2889745514@qq.com>
2023-06-06 11:06:31 +0800

    add:别名联想配置

--- src/vue.d.ts
+++ src/vue.d.ts
@@ -1,4 +1,5 @@
-declare module "*.vue" {
-  import Vue from "@/vue";
-  export default Vue;
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const vueComponent: DefineComponent<{}, {}, any>
+  export default vueComponent
 }
